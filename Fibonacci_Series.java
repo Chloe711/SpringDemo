ba4d394fea707b62b29c57bb59f7e86703d2566e
Chloe711 <66576286+Chloe711@users.noreply.github.com>
2020-06-07 19:51:42 -0700

    Update Fibonacci_Series.java

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -1,4 +1,4 @@
-package coreJava;
+
 
 
 public class Fibonacci_Series {
@@ -6,14 +6,6 @@ public class Fibonacci_Series {
 	public static void main(String[] args) {
 
 		
-		System.out.print(findFibonacciSeries(5));
-	}
-
-	public static int findFibonacciSeries(int n) {
-
-		if (n <= 1) {
-			return n;
-		}
-		return findFibonacciSeries(n - 1) + findFibonacciSeries(n - 2);
+		System.out.print("Hello worls");
 	}
 }
